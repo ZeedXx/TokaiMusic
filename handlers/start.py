830a6f024915711d92d85e9e21afd28c88325ec9
Tokai-Robo <82405146+Tokai-Robo@users.noreply.github.com>
2021-04-28 09:56:35 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -26,20 +26,21 @@ from config import BOT_NAME as bn
 
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
+    await message.reply_sticker("CAACAgQAAx0CVij2LgABECN1YHtruZ8lqFS_yvdqHwkUpjdPUwEAAiMNAALjHT4PZNM7QRBdiYwfBA")
     await message.reply_text(
-        f"""Hello 👋 there! I can play music in voice chats of Telegeam Groups. I have a lot of cool feature that will amaze you!\n\n🔴 Do you want me to play music in your Telegram groups'voice chats? Please click the \'📜 User Manual 📜\' button below to know how you can use me.\n\n🔴 The Assistant must be in your group to play music in the voice chat of your group.\n\n🔴 More info & commands mentioned in the [User Manual](https://telegra.ph/Daisy-X-04-19)\n\nA project by @TeamDaisyX""",
+        f"""Hello!! Aku Adalah Bot Musik Telegram Dengan Banyak Sekali Fitur!\n\n➥ Tolong Baca Manual Dibawah Agar Tau Cara Menggunakanku.\n\n➥ [Assistance](https://t.me/TokaiMusicAssistance) Juga Harus Berada Di Dalam Grup Kamu.\n\nProvided By @Insaynn 🇲🇨""",
         reply_markup=InlineKeyboardMarkup(
             [ 
                 [
                     InlineKeyboardButton(
-                        "📜 User Manual 📜", url="https://telegra.ph/Daisy-X-04-19")
+                        "Manual", url="https://telegra.ph/Music-Bot-Command-04-15")
                   ],[
                     InlineKeyboardButton(
-                        "👨‍💻 Updates 👨‍💻", url="https://t.me/daisyxupdates"
+                        "Group", url="https://t.me/musikalitasID"
                     )
                 ],[ 
                     InlineKeyboardButton(
-                        "Support Chat 🎙️", url="https://t.me/DaisySupport_Official"
+                        "Instagram", url="https://instagram.com/sndykaa/"
                     )]
             ]
         ),
@@ -48,12 +49,12 @@ async def start(_, message: Message):
 
 @Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
-      await message.reply_text("""**🔴 Music player is online**""",
+      await message.reply_text("""**Tokai Music Telah Online**""",
       reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🎙️ Support Group 🎙️", url="https://t.me/daisysupport_Official")
+                        "Owner", url="https://t.me/Insaynn")
                 ]
             ]
         )
