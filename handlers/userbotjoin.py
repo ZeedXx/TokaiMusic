b4f0c098f2a7195f44b48571683371a255f7fe0f
Tokai-Robo <82405146+Tokai-Robo@users.noreply.github.com>
2021-04-28 10:02:28 +0700

    Update userbotjoin.py

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -32,7 +32,7 @@ async def addchannel(client, message):
         invitelink = await client.export_chat_invite_link(chid)
     except:
         await message.reply_text(
-            "<b>Add me as admin of yor group first</b>",
+            "<b>Jadikan Aku Admin Terlebih Dahulu</b>",
         )
         return
 
@@ -43,21 +43,21 @@ async def addchannel(client, message):
 
     try:
         await USER.join_chat(invitelink)
-        await USER.send_message(message.chat.id,"I joined here as you requested")
+        await USER.send_message(message.chat.id,"Aku Telah Disini Seperti Yang Kamu Mau")
     except UserAlreadyParticipant:
         await message.reply_text(
-            "<b>helper already in your chat</b>",
+            "<b>Asisten Bot Sudah Berada Dalam Grup</b>",
         )
         pass
     except Exception as e:
         print(e)
         await message.reply_text(
-            f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group."
-            "\n\nOr manually add @DaisyXhelper to your Group and try again</b>",
+            f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} Tidak Dapat Join Kedalam Grup Karena Masalah Flood! Pastikan User Tidak Dibanned Digrup."
+            "\n\nAtau Tambahkan @TokaiMusicAssistance Secara Manual Dan Coba Lagi</b>",
         )
         return
     await message.reply_text(
-            "<b>helper userbot joined your chat</b>",
+            "<b>Asisten Bot Berhasil Join Ke Grup</b>",
         )
     
 @USER.on_message(filters.group & filters.command(["userbotleave"]))
@@ -66,7 +66,7 @@ async def rem(USER, message):
         await USER.leave_chat(message.chat.id)
     except:  
         await message.reply_text(
-            f"<b>User couldn't leave your group! May be floodwaits."
-            "\n\nOr manually kick me from to your Group</b>",
+            f"<b>User Tidak Bisa Meninggalkan Grup! Mungkin Karena Floodwaits."
+            "\n\nAtau Kick Secara Manual</b>",
         )
         return
