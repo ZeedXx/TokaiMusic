85a4e21282e0c45c5d4d617cf3e1874b2bcff6d5
Tokai-Robo <82405146+Tokai-Robo@users.noreply.github.com>
2021-04-28 08:22:25 +0700

    Update inline.py

--- handlers/inline.py
+++ handlers/inline.py
@@ -13,7 +13,7 @@ async def inline(client: Client, query: InlineQuery):
         await client.answer_inline_query(
             query.id,
             results=answers,
-            switch_pm_text="Type a YouTube video name...",
+            switch_pm_text="Ketik Judul Lagu...",
             switch_pm_parameter="help",
             cache_time=0
         )
@@ -46,6 +46,6 @@ async def inline(client: Client, query: InlineQuery):
             await query.answer(
                 results=answers,
                 cache_time=0,
-                switch_pm_text="Error: Search timed out",
+                switch_pm_text="Error: Waktu Server Habis",
                 switch_pm_parameter="",
             )
