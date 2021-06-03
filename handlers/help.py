fc42eb333772ae8ec0b7a3efa9e0d972918f7472
ZeedXx <85275213+ZeedXx@users.noreply.github.com>
2021-06-03 14:03:36 +0700

    Update help.py

--- handlers/help.py
+++ handlers/help.py
@@ -7,25 +7,25 @@ from config import BOT_NAME as bn
 async def start(_, message: Message):
       await message.reply_text("""_Perintah Untuk Semua Member_ :
 ┈──────────────────────┈
-/play <Judul>  - _Memutar Lagu Melalui Youtube._
-/dplay <Judul>  - _Memutar Lagu Melalui Deezer._
-/reply - _Memutar lagu lewat reply link YouTube atau file audio._
-/playlist - _Untuk Menampilkan Daftar Putar Lagu._
-/song <Judul> - _Untuk Mendownload Lagu di YouTube._
-/vsong <Judul> - _Mendownload Video dari YouTube._
-/deezer <Judul> - _Mendownload Lagu Dari Deezer._
-/saavn <Judul> - _Mendownload Lagu Dari Saavn._
-/search <Judul> - _Mencari Video di YouTube Secara Detail._
-/current - _Mengecek Antrian di Grup Kamu._
-/player - _Mengelola Bot di Grup Kamu._
+× /play <Judul>  : _Memutar Lagu Melalui Youtube._
+× /dplay <Judul> : _Memutar Lagu Melalui Deezer._
+× /reply <Link>  : _Memutar lagu lewat reply link YouTube atau file audio._
+× /playlist : _Untuk Menampilkan Daftar Putar Lagu._
+× /song <Judul>  : _Untuk Mendownload Lagu di YouTube._
+× /vsong <Judul> : _Mendownload Video dari YouTube._
+× /deezer <Judul> : _Mendownload Lagu Dari Deezer._
+× /saavn <Judul>  : _Mendownload Lagu Dari Saavn._
+× /search <Judul> : _Mencari Video di YouTube Secara Detail._
+× /current : _Mengecek Antrian di Grup Kamu._
+× /player  : _Mengelola Bot di Grup Kamu._
 
 _Perintah Untuk Admin Group_ :
 ┈──────────────────────┈
-/pause - _Menjeda Lagu._
-/resume - _Melanjutkan Lagu Yang Dipause._
-/skip - _Menloncati Lagu ke Lagu Berikutnya._
-/end - _Memberhentikan Lagu._
-/joingroup - _Mengundang Asisten ke VCG._
-/leavegroup - _Menendang Asisten Music dari VCG._
-/adminreset - _Memperbarui Admin List._
-/admincache - _Me-Refresh Cache Admin Pada Bot._""")
+× /pause  : _Menjeda Lagu._
+× /resume : _Melanjutkan Lagu Yang Dipause._
+× /skip : _Menloncati Lagu ke Lagu Berikutnya._
+× /end  : _Memberhentikan Lagu._
+× /joingroup  : _Mengundang Asisten ke VCG._
+× /leavegroup : _Menendang Asisten Music dari VCG._
+× /adminreset : _Memperbarui Admin List._
+× /admincache : _Me-Refresh Cache Admin Pada Bot._""")
